d304c84fbf33fc06285b988d055dcd00f3c67c51
Athansya <atoriz98@outlook.com>
2021-02-03 11:09:53 -0600

    Added Header

---
+++ Scripts/histogram.py
@@ -0,0 +1,15 @@
+'''
+Filename: /home/atoriz98/PythonProjects/ImageProcessing/Scripts/histogram.py
+Path: /home/atoriz98/PythonProjects/ImageProcessing/Scripts
+Created Date: Wednesday, February 3rd 2021, 10:10:41 am
+Author: Athansya
+
+Copyright (c) 2021 Your Company
+
+Description:
+Histograms provide information about the distribution of the intensity values of an image and are frequently used in image segmentation and in image enhancement.
+- Maier, A., Steidl, S., Christlein, V. and Hornegger, J., 2018. Medical Imaging Systems. 1st ed. Springer International Publishing, p.38.
+'''
+import PIL, numpy as np
+
+
